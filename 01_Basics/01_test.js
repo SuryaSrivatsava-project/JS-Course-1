c0a38dadd82288d8ea75fc64942bd074ea96bdca
SuryaSrivatsava-project <147935997+SuryaSrivatsava-project@users.noreply.github.com>
2024-01-24 07:21:04 +0000

    data types

---
+++ 01_Basics/01_test.js
@@ -0,0 +1,11 @@
+const accountId = 144553;
+let accountEmail = 'surya@google.com';
+var accountPassword = '12345'
+accountCity = 'Delhi';
+let accountState;
+// accountId = 2; not allowed to rename const variables
+accountEmail = "hc@hc.com"
+accountPassword = '212121'
+accountCity = 'Bangalore'
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version
+// alert("hello") we are using nodejs not browser 
+let name = 'surya'
+let age = 26
+let isLoggedIn = true
+// number
+// string 
+// bigint
+// boolean
+// null => standalone value which defines intentionally by programmer that its empty
+// undefined
+// symbol
+
+// object types
+
+console.log(typeof 12n)
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
