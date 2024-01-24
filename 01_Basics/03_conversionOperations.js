9066899a13b0b92fa1014d5cf5c1b22498cf4173
SuryaSrivatsava-project <147935997+SuryaSrivatsava-project@users.noreply.github.com>
2024-01-24 10:46:23 +0000

    3 and 4 done

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -1,10 +1,10 @@
 let score = "33";
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 // Numbers
 let valueInNumber = Number(score);
-console.log(typeof(valueInNumber));
+// console.log(typeof(valueInNumber));
 
 // "33" => 33
 // "33abc" => NAN but type is number
@@ -15,7 +15,7 @@ console.log(typeof(valueInNumber));
 // Booleans
 let isLoggedIn = " ";
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(typeof booleanIsLoggedIn,booleanIsLoggedIn)
+// console.log(typeof booleanIsLoggedIn,booleanIsLoggedIn)
 
 // 1 => true
 // 0 => false
@@ -24,4 +24,36 @@ console.log(typeof booleanIsLoggedIn,booleanIsLoggedIn)
 
 let someNumber = 23;
 let stringNumber = String(someNumber);
-console.log(stringNumber,typeof stringNumber)
+// console.log(stringNumber,typeof stringNumber)
+
+
+/*******************************Operations************************* */
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+let str1 = 'Hello';
+let str2 = ' Surya';
+// console.log(str1+str2)
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+console.log(+true); // 1
+console.log(+""); // 0
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2;
+console.log(num1,num2,num3);
+
+let gameCounter = 99;
+gameCounter++;
+console.log(gameCounter)
+
---
+++ 01_Basics/04_comparisionOperators.js
@@ -0,0 +1,25 @@
+// console.log(2>1);
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2<=1)
+// console.log(2==1)
+// console.log(2!=1)
+// true for below
+// console.log("2">1) 
+// console.log("02">1) 
+
+// comparision operators work differently than  equality operator
+// >,< converts null to 0 so null>=0 is true
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+
+// All false for undefined
+// console.log(undefined>0)
+// console.log(undefined==0)
+// console.log(undefined>=0)
+
+// Strict check === and !==
+console.log("2" === 2);
+console.log(2>true);
+console.log(2!==1);
