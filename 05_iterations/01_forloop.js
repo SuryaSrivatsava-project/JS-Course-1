847baf88586be25deb98f363423ae80c80ed6134
SuryaSrivatsava-project <147935997+SuryaSrivatsava-project@users.noreply.github.com>
2024-01-27 11:25:08 +0000

    loops

---
+++ 05_iterations/01_forloop.js
@@ -0,0 +1,29 @@
+// for loop
+// for(let index=0;index<=10;index++){
+//     console.log(index)
+// }
+// var element in for loop can be accessible outside
+// for(let i=0;i<=10;i++){
+//     console.log(`Outer loop value ${i}`);
+//     for(let j=0;j<=10;j++){
+//         console.log(`Inner loop value ${j}`);
+//     }
+// }
+
+// let myarray  = ['spiderman','superman','batman'];
+// for (let index = 0; index < myarray.length; index++) {
+//     const element = myarray[index];
+//     console.log(element)
+// }
+
+// break and continue
+
+for(let i=1;i<=20;i++){
+    // if(i==5){
+    //     break; // breaks and exits the loop
+    // }
+    // if(i==5){
+    //     continue; // skips the next steps and moves to next iteration
+    // }
+    console.log(`Value of i is ${i}`)
+}
---
+++ 05_iterations/02_while&dowhileloop.js
@@ -0,0 +1,20 @@
+// while loop
+// let index = 0;
+// while(index<=10){
+//     console.log(index);
+//     index+= 2;
+// }
+
+// let myarray  = ['spiderman','superman','batman'];
+// let i = 0;
+// while(i<myarray.length){
+//     console.log(myarray[i]);
+//     i++;
+// }
+
+// In do-while loop it executes atleast once
+let score = 1;
+do{
+    console.log(`Score is ${score}`);
+    score++;
+}while(score<=10);
