9169e9c2bc8d14c60deab64fc46fd5b401ae8b4a
SuryaSrivatsava-project <147935997+SuryaSrivatsava-project@users.noreply.github.com>
2024-02-11 11:58:13 +0000

    all changes

---
+++ 09_advance_one/callbacks.js
@@ -0,0 +1,16 @@
+console.log('Namaste');
+setTimeout(function(){
+    console.log('Javascript')
+},5000)
+console.log("Hello there")
+
+/*
+* Callbacks
+ * Async way of writing code
+ * Passing functions as params to function
+* Callback hell 
+ * In callback hell we can face inversion of control
+ * It means we dont know if call back is executing properly or not or if internal callbacks are running or not
+ * Dev's will not have any control
+ 
+ */
---
+++ 09_advance_one/promisesDepth.js
@@ -0,0 +1,39 @@
+/** Promises
+ * Promises are used to handle async operations
+ * Rather passing a function as arg or as a callback we attach callback function to Promise object using this
+ */
+
+const cart = ["shoes","pants","kurta"];
+// To write in callbacks, we wrap the function inside a function and pass to responsible function
+function createOrder(cart,callback){
+    callback(cart)
+}
+createOrder(1,proceedtoPayment)
+function proceedtoPayment(id){
+    console.log(id)
+}
+// Promises
+const promise = createOrder(cart); // this returns a promise
+promise.then(function(orderID){
+    proceedtoPayment(orderID);
+})
+
+// Always return a promise chain
+
+createOrder(cart).then(function(orderID){
+    return proceedtoPayment(orderID)
+}).then(function(paymentInfo){
+    return showOrderSummary(paymentInfo)
+}).then(function(paymentInfo){
+    return updateWalletBalance(paymentInfo)
+})
+//Interview questions
+/**
+ * what is a promise?
+ * Its an object which represents eventual completion of Async Operation
+ * States of promise?
+ * Pending, Fulfilled and rejected
+ *  
+
+ */
+
